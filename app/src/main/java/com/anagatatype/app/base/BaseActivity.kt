9a5609660bbed138d15678c2634c02e51036fe42
khyvuthisak <khyvuthisak@gmail.com>
2021-05-25 10:46:07 +0700

    Update view main toolbar

---
+++ app/src/main/java/com/anagatatype/app/base/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.anagatatype.app.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<B : ViewBinding> : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/anagatatype/app/features/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anagatatype.app.features.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anagatatype.app.base.BaseActivity
+import com.anagatatype.app.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
