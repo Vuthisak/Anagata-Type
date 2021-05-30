fc57507092a23ff58139fa9fd05a183b5e8f4752
khyvuthisak <khyvuthisak@gmail.com>
2021-05-30 22:54:14 +0700

    update detail

--- app/src/main/java/com/anagata/typingkit/features/detail/DetailFragment.kt
+++ app/src/main/java/com/anagata/typingkit/features/detail/DetailFragment.kt
@@ -2,6 +2,7 @@ package com.anagata.typingkit.features.detail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.anagata.typingkit.R
 import com.anagata.typingkit.base.BaseFragment
@@ -18,10 +19,21 @@ class DetailFragment : BaseFragment(R.layout.fragment_detail) {
     }
 
     private fun setupContent() {
+        setupFontSizeDropDown()
         binding.detailRecycler.run {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = DetailAdapter(arrayListOf())
         }
+    }
+
+    private fun setupFontSizeDropDown() {
+        val sizes = requireContext().resources.getStringArray(R.array.sizes)
+        val adapter = ArrayAdapter(requireContext(), R.layout.item_drop_down, sizes)
+        binding.sizeAutoCompleteText.run {
+            setAdapter(adapter)
+            setText(adapter.getItem(0), false)
+        }
+
     }
 
 }
